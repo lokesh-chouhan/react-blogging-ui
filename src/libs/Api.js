8a024090f41d3a7c9c896e2c26a3fa7debd2bc01
Shashi99rocky <shashikant.marskole@gmail.com>
2022-10-08 18:09:49 +0530

    pages , post, and fetches are done

--- src/libs/Api.js
+++ src/libs/Api.js
@@ -1,13 +1,14 @@
 const API_URL = "https://dummy.restapiexample.com/api/v1";
 
 const fetchData = () => {
-
 }
 
 export const fetchBloglist = () => {
     return fetch(`${API_URL}/employees`).then(res => res.json()).then(res => res.data);
+
 }
 
 export const fetchPost = (id) => {
     return fetch(`${API_URL}/employee/${id}`).then(res => res.json()).then(res => res.data);
+    
 }
